3fd8b0d37cf93c1539f49c099ef0560faeea9cb1
etluconk <edson@edsonjames.com>
2025-09-23 12:58:30 -0700

    Implemented state_machine, attacking and hitstun for base_character.gd

--- systems/characters/base_character.gd
+++ systems/characters/base_character.gd
@@ -2,3 +2,117 @@ extends CharacterBody2D
 class_name Character
 
 @export var player_idx = 0
+@onready var gravity_comp : GravityComp = $%GravityComp
+@onready var horizontal_movement_comp : HorizontalMovementComp = $%HorizontalMovementComp
+@onready var jump_comp : JumpComp = $%JumpComp
+
+@onready var frame_data_system : FrameDataSystem = $%FrameDataSystem
+@onready var move_activation_handler : MoveActivationHandler = $%MoveActivationHandler
+@onready var sprite : Sprite2D = $Sprite2D
+
+var state_machine : CallableStateMachine = CallableStateMachine.new()
+var horizontal_dir_facing := 1
+@export var state_properties : StateProperties
+
+var hitstun_knockback_vector : Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	state_machine.add_states(state_grounded, enter_state_grounded, leave_state_grounded)
+	state_machine.add_states(state_attack, enter_state_attack, leave_state_attack)
+	state_machine.add_states(state_hitstun, enter_state_hitstun, leave_state_hitstun)
+	state_machine.set_initial_state(state_grounded)
+
+	frame_data_system.took_a_hit.connect(on_took_a_hit)
+
+func _physics_process(_delta: float) -> void:
+	state_machine.update()
+
+func update_horizontal_dir_facing() -> void:
+	horizontal_dir_facing = horizontal_movement_comp.horizontal_dir_facing
+	frame_data_system.scale.x = float(horizontal_dir_facing)
+	sprite.flip_h = (horizontal_dir_facing == -1)
+
+func on_took_a_hit(attacking_move_frame : MoveFrame) -> void:
+	match state_machine.current_state:
+		"state_grounded":
+			state_machine.change_state(state_hitstun)
+			set_hitstun_knockback_vector(attacking_move_frame)
+		"state_attack":
+			state_machine.change_state(state_hitstun)
+			set_hitstun_knockback_vector(attacking_move_frame)
+
+func set_hitstun_knockback_vector(attacking_move_frame : MoveFrame) ->  void:
+	var kb_vector = attacking_move_frame.state_properties.knockback_vector
+	hitstun_knockback_vector = Vector2(
+		kb_vector.x * attacking_move_frame.get_character().horizontal_dir_facing,
+		kb_vector.y)
+
+func apply_knockback() -> void:
+	velocity = hitstun_knockback_vector
+
+
+
+func enter_state_grounded() -> void:
+	# print("player " + str(player_idx) + " entering grounded state")
+	pass
+
+func state_grounded() -> void:
+	gravity_comp.enabled = true
+	horizontal_movement_comp.enabled = true
+	jump_comp.enabled = true
+
+	move_and_slide()
+
+	if !state_properties.horizontal_dir_locked:
+		update_horizontal_dir_facing()
+	if state_properties.attack:
+		state_machine.change_state(state_attack)
+
+func leave_state_grounded() -> void:
+	# print("player " + str(player_idx) + " leaving grounded state")
+	pass
+
+
+
+func enter_state_attack() -> void:
+	# print("player " + str(player_idx) + " entering attack state")
+	pass
+
+func state_attack() -> void:
+	if state_properties.locked:
+		gravity_comp.enabled = false
+		horizontal_movement_comp.enabled = false
+		jump_comp.enabled = false
+		velocity = Vector2.ZERO
+	else:
+		gravity_comp.enabled = true
+		horizontal_movement_comp.enabled = true
+		jump_comp.enabled = true
+		move_and_slide()
+
+	if !state_properties.horizontal_dir_locked:
+		update_horizontal_dir_facing()
+
+	if !state_properties.attack:
+		state_machine.change_state(state_grounded)
+
+func leave_state_attack() -> void:
+	# print("player " + str(player_idx) + " leaving attack state")
+	pass
+
+
+func enter_state_hitstun() -> void:
+	# print("player " + str(player_idx) + " entering hitstun state")
+	move_activation_handler.go_to_hitstun()
+
+func state_hitstun() -> void:
+	gravity_comp.enabled = false
+	horizontal_movement_comp.enabled = false
+	jump_comp.enabled = false
+	apply_knockback()
+	move_and_slide()
+	# state_machine.change_state(state_grounded)
+
+func leave_state_hitstun() -> void:
+	# print("player " + str(player_idx) + " leaving hitstun state")
+	pass

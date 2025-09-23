83b84b499a4291c788de89b63e0d405e9a66e3a5
etluconk <edson@edsonjames.com>
2025-09-23 12:46:15 -0700

    Re-implemented InputPlayerIdxComp's functionality invidividually in platforming_base.gd

--- homemade_systems/platforming/platforming_base.gd
+++ homemade_systems/platforming/platforming_base.gd
@@ -1,16 +1,21 @@
 extends Component
 class_name PlatformingBaseComponent
 
-@export var input_player_index_component : InputPlayerIdxComp
-
 func horizontal_input_direction() -> float:
-	return Input.get_axis("left_" + str(get_player_idx()), "right_" + str(get_player_idx()))
+	if get_player_idx() == -1: return 0
+	return int(Input.is_action_pressed("right_" + str(get_player_idx()))) - int(Input.is_action_pressed("left_" + str(get_player_idx())))
 
 func player_input_just_pressed(input_action_name : String) -> bool:
+	if get_player_idx() == -1: return false
 	return Input.is_action_just_pressed(input_action_name + "_" + str(get_player_idx()))
 
 func player_input_pressed(input_action_name : String) -> bool:
+	if get_player_idx() == -1: return false
 	return Input.is_action_pressed(input_action_name + "_" + str(get_player_idx()))
 
 func get_player_idx() -> int:
-	return input_player_index_component.player_idx
+	var p = get_parent()
+	if p is Character:
+		return p.player_idx
+	else:
+		return -1

7520e43f9725f8db08b4137752b271730e27428b
etluconk <edson@edsonjames.com>
2025-09-26 13:01:07 -0700

    Implemented hitstun_knockback_time_remaining

--- systems/characters/base_character.gd
+++ systems/characters/base_character.gd
@@ -16,6 +16,9 @@ var horizontal_dir_facing := 1
 @export var state_properties : StateProperties
 
 var hitstun_knockback_vector : Vector2 = Vector2.ZERO
+var hitstun_knockback_time_remaining : float = 0.0
+
+var invinsibility_time_remaining : float = 0.0
 
 func _ready() -> void:
 	state_machine.add_states(state_grounded, enter_state_grounded, leave_state_grounded)
@@ -25,8 +28,8 @@ func _ready() -> void:
 
 	frame_data_system.took_a_hit.connect(on_took_a_hit)
 
-func _physics_process(_delta: float) -> void:
-	state_machine.update()
+func _physics_process(delta: float) -> void:
+	state_machine.update(delta)
 
 func update_horizontal_dir_facing() -> void:
 	horizontal_dir_facing = horizontal_movement_comp.horizontal_dir_facing
@@ -34,13 +37,17 @@ func update_horizontal_dir_facing() -> void:
 	sprite.flip_h = (horizontal_dir_facing == -1)
 
 func on_took_a_hit(attacking_move_frame : MoveFrame) -> void:
+	if invinsibility_time_remaining > 0: return
+
 	match state_machine.current_state:
 		"state_grounded":
 			state_machine.change_state(state_hitstun)
 			set_hitstun_knockback_vector(attacking_move_frame)
+			hitstun_knockback_time_remaining = attacking_move_frame.state_properties.hitstun_knockback_frames / 60.0
 		"state_attack":
 			state_machine.change_state(state_hitstun)
 			set_hitstun_knockback_vector(attacking_move_frame)
+			hitstun_knockback_time_remaining = attacking_move_frame.state_properties.hitstun_knockback_frames / 60.0
 
 func set_hitstun_knockback_vector(attacking_move_frame : MoveFrame) ->  void:
 	var kb_vector = attacking_move_frame.state_properties.knockback_vector
@@ -57,9 +64,10 @@ func enter_state_grounded() -> void:
 	# print("player " + str(player_idx) + " entering grounded state")
 	pass
 
-func state_grounded() -> void:
+func state_grounded(_delta : float) -> void:
 	gravity_comp.enabled = true
 	horizontal_movement_comp.enabled = true
+	horizontal_movement_comp.player_has_control = true
 	jump_comp.enabled = true
 
 	move_and_slide()
@@ -82,15 +90,17 @@ func enter_state_attack() -> void:
 	# print("player " + str(player_idx) + " entering attack state")
 	pass
 
-func state_attack() -> void:
+func state_attack(_delta : float) -> void:
 	if state_properties.locked:
 		gravity_comp.enabled = false
 		horizontal_movement_comp.enabled = false
+		horizontal_movement_comp.player_has_control = true
 		jump_comp.enabled = false
 		velocity = Vector2.ZERO
 	else:
 		gravity_comp.enabled = true
 		horizontal_movement_comp.enabled = true
+		horizontal_movement_comp.player_has_control = true
 		jump_comp.enabled = true
 		move_and_slide()
 
@@ -112,13 +122,18 @@ func enter_state_hitstun() -> void:
 	if animation_player is AnimationPlayer:
 		animation_player.play("Hitstun")
 
-func state_hitstun() -> void:
-	gravity_comp.enabled = false
-	horizontal_movement_comp.enabled = false
+func state_hitstun(delta : float) -> void:
+	gravity_comp.enabled = true
+	horizontal_movement_comp.enabled = true
+	horizontal_movement_comp.player_has_control = false
 	jump_comp.enabled = false
 	apply_knockback()
 	move_and_slide()
-	# state_machine.change_state(state_grounded)
+
+	hitstun_knockback_time_remaining -= delta
+
+	if hitstun_knockback_time_remaining <= 0:
+		state_machine.change_state(state_grounded)
 
 func leave_state_hitstun() -> void:
 	# print("player " + str(player_idx) + " leaving hitstun state")
